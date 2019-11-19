7ad46d3adba7f276a2db8a034e0921956000abf0
Andrew Kirwin <andrew.kirwin@zestia.com>
2019-11-19 09:04:24 +0000

    initial commit

--- tests/helpers/wait-for-transition.js
+++
@@ -1,8 +0,0 @@
-import { find } from '@ember/test-helpers';
-import { Promise } from 'rsvp';
-
-export default function waitForTransition(selector) {
-  return new Promise(resolve => {
-    find(selector).addEventListener('transitionend', resolve, { once: true });
-  });
-}
