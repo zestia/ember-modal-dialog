1c999fcc3f40207b1e01c94e598ffa81882619ba
Andrew Kirwin <andrew.kirwin@zestia.com>
2019-11-27 14:55:17 +0000

    add dummy modal

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,10 +1,16 @@
 import Controller from '@ember/controller';
+import { action, set } from '@ember/object';
 
-export default Controller.extend({
-  show: true,
-  actions: {
-    toggle() {
-      this.toggleProperty('show');
-    }
+export default class ApplicationController extends Controller {
+  showTestModal = false;
+
+  @action
+  openTestModal() {
+    set(this, 'showTestModal', true);
   }
-});
+
+  @action
+  closeTestModal() {
+    set(this, 'showTestModal', false);
+  }
+}
