0652de14717ac1785336dbf8ce02fd2f41a7c076
Andrew Kirwin <github@andrewkirwin.co.uk>
2023-02-21 13:37:17 +0000

    update test helper

--- tests/helpers/wait-for-animation.js
+++ tests/helpers/wait-for-animation.js
@@ -1,12 +1,8 @@
 import { find } from '@ember/test-helpers';
 import { waitForAnimation } from '@zestia/animation-utils';
 
-export default async function (selector, options) {
+export default function (selector, options) {
   const element = typeof selector === 'string' ? find(selector) : selector;
 
-  const animations = await waitForAnimation(element, options);
-
-  if (animations.length < 1) {
-    throw new Error('No animations performed');
-  }
+  return waitForAnimation(element, options);
 }
