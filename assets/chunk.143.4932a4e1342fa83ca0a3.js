b07e800bdeb98c7fd3cce748e006da380a2d7621
Andrew Kirwin <github@andrewkirwin.co.uk>
2023-07-04 11:45:00 +0100

    Deploy gh-pages from commit de7f5ab4c9b3bc2dcff87a6ea13406001256360b

---
+++ assets/chunk.143.4932a4e1342fa83ca0a3.js
@@ -0,0 +1,31 @@
+var __ember_auto_import__;(()=>{var e={259:(e,t,n)=>{"use strict"
+async function o(e){let t=arguments.length>1&&void 0!==arguments[1]?arguments[1]:{}
+if("function"==typeof e.getAnimations)return n().length<1&&!0!==t.maybe?await new Promise((t=>{e.addEventListener("animationstart",t,{once:!0}),e.addEventListener("transitionstart",t,{once:!0})})):await new Promise(window.requestAnimationFrame),Promise.allSettled(n().map((e=>e.finished)))
+function n(){return e.getAnimations({subtree:t.subtree}).filter((e=>t.transitionProperty?e.transitionProperty===t.transitionProperty:!t.animationName||e.animationName===t.animationName))}}n.r(t),n.d(t,{waitForAnimation:()=>o})},863:(e,t,n)=>{"use strict"
+n.r(t),n.d(t,{clearAllBodyScrollLocks:()=>p,disableBodyScroll:()=>v,enableBodyScroll:()=>y})
+var o=!1
+if("undefined"!=typeof window){var i={get passive(){o=!0}}
+window.addEventListener("testPassive",null,i),window.removeEventListener("testPassive",null,i)}var r="undefined"!=typeof window&&window.navigator&&window.navigator.platform&&(/iP(ad|hone|od)/.test(window.navigator.platform)||"MacIntel"===window.navigator.platform&&window.navigator.maxTouchPoints>1),a=[],l=!1,c=-1,s=void 0,d=void 0,u=function(e){return a.some((function(t){return!(!t.options.allowTouchMove||!t.options.allowTouchMove(e))}))},f=function(e){var t=e||window.event
+return!!u(t.target)||t.touches.length>1||(t.preventDefault&&t.preventDefault(),!1)},m=function(){void 0!==d&&(document.body.style.paddingRight=d,d=void 0),void 0!==s&&(document.body.style.overflow=s,s=void 0)},v=function(e,t){if(e){if(!a.some((function(t){return t.targetElement===e}))){var n={targetElement:e,options:t||{}}
+a=[].concat(function(e){if(Array.isArray(e)){for(var t=0,n=Array(e.length);t<e.length;t++)n[t]=e[t]
+return n}return Array.from(e)}(a),[n]),r?(e.ontouchstart=function(e){1===e.targetTouches.length&&(c=e.targetTouches[0].clientY)},e.ontouchmove=function(t){1===t.targetTouches.length&&function(e,t){var n=e.targetTouches[0].clientY-c
+!u(e.target)&&(t&&0===t.scrollTop&&n>0||function(e){return!!e&&e.scrollHeight-e.scrollTop<=e.clientHeight}(t)&&n<0?f(e):e.stopPropagation())}(t,e)},l||(document.addEventListener("touchmove",f,o?{passive:!1}:void 0),l=!0)):function(e){if(void 0===d){var t=!!e&&!0===e.reserveScrollBarGap,n=window.innerWidth-document.documentElement.clientWidth
+t&&n>0&&(d=document.body.style.paddingRight,document.body.style.paddingRight=n+"px")}void 0===s&&(s=document.body.style.overflow,document.body.style.overflow="hidden")}(t)}}else console.error("disableBodyScroll unsuccessful - targetElement must be provided when calling disableBodyScroll on IOS devices.")},p=function(){r?(a.forEach((function(e){e.targetElement.ontouchstart=null,e.targetElement.ontouchmove=null})),l&&(document.removeEventListener("touchmove",f,o?{passive:!1}:void 0),l=!1),c=-1):m(),a=[]},y=function(e){e?(a=a.filter((function(t){return t.targetElement!==e})),r?(e.ontouchstart=null,e.ontouchmove=null,l&&0===a.length&&(document.removeEventListener("touchmove",f,o?{passive:!1}:void 0),l=!1)):a.length||m()):console.error("enableBodyScroll unsuccessful - targetElement must be provided when calling enableBodyScroll on IOS devices.")}},304:(e,t,n)=>{"use strict"
+n.r(t),n.d(t,{default:()=>c,modifier:()=>d})
+const o=require("@ember/application"),i=require("@ember/modifier"),r=require("@ember/destroyable")
+function a(e,t,n){return t in e?Object.defineProperty(e,t,{value:n,enumerable:!0,configurable:!0,writable:!0}):e[t]=n,e}class l{constructor(e){this.owner=e,a(this,"capabilities",(0,i.capabilities)("3.22"))}createModifier(e,t){return{instance:new e(this.owner,t),element:null}}installModifier(e,t,n){const o=function(e,t){const n=e
+return n.element=t,n}(e,t)
+o.instance.modify(t,n.positional,n.named)}updateModifier(e,t){e.instance.modify(e.element,t.positional,t.named)}destroyModifier(e){let{instance:t}=e;(0,r.destroy)(t)}}class c{constructor(e,t){(0,o.setOwner)(this,e)}modify(e,t,n){}}(0,i.setModifierManager)((e=>new l(e)),c)
+const s=new class{constructor(){a(this,"capabilities",(0,i.capabilities)("3.22"))}createModifier(e){return{element:null,instance:e}}installModifier(e,t,n){const o=function(e,t){const n=e
+return n.element=t,n}(e,t),{positional:i,named:r}=n,a=e.instance(t,i,r)
+"function"==typeof a&&(o.teardown=a)}updateModifier(e,t){"function"==typeof e.teardown&&e.teardown()
+const n=e.instance(e.element,t.positional,t.named)
+"function"==typeof n&&(e.teardown=n)}destroyModifier(e){"function"==typeof e.teardown&&e.teardown()}}
+function d(e){return(0,i.setModifierManager)((()=>s),e)}},73:(e,t,n)=>{var o,i
+e.exports=(o=_eai_d,i=_eai_r,window.emberAutoImportDynamic=function(e){return 1===arguments.length?i("_eai_dyn_"+e):i("_eai_dynt_"+e)(Array.prototype.slice.call(arguments,1))},window.emberAutoImportSync=function(e){return i("_eai_sync_"+e)(Array.prototype.slice.call(arguments,1))},o("@zestia/animation-utils",[],(function(){return n(259)})),o("body-scroll-lock",[],(function(){return n(863)})),void o("ember-modifier",[],(function(){return n(304)})))},439:function(e,t){window._eai_r=require,window._eai_d=define}},t={}
+function n(o){var i=t[o]
+if(void 0!==i)return i.exports
+var r=t[o]={exports:{}}
+return e[o].call(r.exports,r,r.exports,n),r.exports}n.d=(e,t)=>{for(var o in t)n.o(t,o)&&!n.o(e,o)&&Object.defineProperty(e,o,{enumerable:!0,get:t[o]})},n.o=(e,t)=>Object.prototype.hasOwnProperty.call(e,t),n.r=e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n(439)
+var o=n(73)
+__ember_auto_import__=o})()
