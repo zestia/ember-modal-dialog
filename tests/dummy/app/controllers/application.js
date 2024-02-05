3da63e5a81cc92312796f6386826a53d48abe6f7
Andrew Kirwin <github@andrewkirwin.co.uk>
2024-02-05 13:57:52 +0000

    convert to native dialog

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,16 +1,10 @@
-/* eslint-disable no-alert */
-
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import { later } from '@ember/runloop';
 
 export default class ApplicationController extends Controller {
   @tracked isEscapable = false;
-  @tracked clickOutsideToEscape = true;
   @tracked loadDelay = false;
-  @tracked confirmEscape = false;
-  @tracked escapeOnFocusLeave = false;
   @tracked showExampleModal = false;
 
   @action
@@ -24,30 +18,8 @@ export default class ApplicationController extends Controller {
   }
 
   @action
-  resetClickOutsideToEscape() {
-    this.clickOutsideToEscape = true;
-  }
-
-  @action
-  handleEscapeExampleModal(modal, event) {
-    const escapable =
-      this.isEscapable &&
-      ((this.clickOutsideToEscape && event instanceof MouseEvent) ||
-        event instanceof KeyboardEvent);
-
-    if (!escapable) {
-      return;
-    }
-
-    let close = true;
-
-    if (this.confirmEscape) {
-      close = confirm('Are you sure?');
-    }
-
-    if (close) {
-      modal.close();
-    }
+  setEscapable(event) {
+    this.isEscapable = event.target.checked;
   }
 
   @action
@@ -56,8 +28,6 @@ export default class ApplicationController extends Controller {
       return;
     }
 
-    return new Promise((resolve) => {
-      later(resolve, 2000);
-    });
+    return new Promise((resolve) => setTimeout(resolve, 2000));
   }
 }
